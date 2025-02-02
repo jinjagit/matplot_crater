ce48c0d67bb380b15b9fc84a0160aa7afb8eb2b2
Simon Tharby <simontharby@gmail.com>
2025-02-02 19:29:57 +0000

    add sliders for radius and depth vars

--- parabola.py
+++ parabola.py
@@ -2,13 +2,20 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.widgets import Slider
 
-# Create x axis with range and y axis with Sine
-# Function for Plotting Sine Graph
+def update_plot(val):
+   r = radius_slider.val
+   a = depth_slider.val
+   y = a * (x**2 - r**2)
+   line.set_ydata(y)
+   fig.canvas.draw_idle()
+
+fig, ax = plt.subplots()
+plt.subplots_adjust(bottom=0.2)
 
 r = 1.0 # Radius
 a = 0.5 # depth of parabola (below zero)
-
 
 x = np.arange(-np.pi, np.pi, 0.1)
 y = a * (x**2 - r**2)
@@ -18,6 +25,14 @@ plt.ylim(-2, 2)
 plt.axhline(0, color='gray', linewidth=0.5)
 plt.axvline(0, color='gray', linewidth=0.5)
 
+# Add sliders
+ax_radius = plt.axes([0.1, 0.1, 0.65, 0.03], facecolor='lightgoldenrodyellow')
+radius_slider = Slider(ax_radius, 'radius', 0, 2.0, valinit=1.0)
+ax_depth = plt.axes([0.1, 0.05, 0.65, 0.03], facecolor='lightgoldenrodyellow')
+depth_slider = Slider(ax_depth, 'depth', 0, 2.0, valinit=0.5)
+
 # Plot Sine Graph
-plt.plot(x, y, color='green')
+line, = ax.plot(x, y, color='green')
+radius_slider.on_changed(update_plot)
+depth_slider.on_changed(update_plot)
 plt.show()
